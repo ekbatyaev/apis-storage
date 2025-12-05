c002f17c9dc69530400693c21f7caae8a9f8629a
ekbatyaev <161816517+ekbatyaev@users.noreply.github.com>
2025-12-05 22:36:16 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home/Home';
 
+// Компонент для определения текущей страницы
+const PageHandler = () => {
+  const location = useLocation();
+  
+  useEffect(() => {
+    // Определяем, какая страница должна отображаться
+    const path = location.pathname;
+    
+    if (path.includes('places')) {
+      // Загружаем содержимое places.html
+      console.log('Loading places page');
+    }
+    // и т.д.
+  }, [location]);
+  
+  return null;
+};
+
 function App() {
-  return <Home />;
+  return (
+    <Router basename="/apis-storage">
+      <PageHandler />
+      <Routes>
+        <Route path="/*" element={<Home />} />
+        {/* Все остальные пути также ведут на Home */}
+        <Route path="/places" element={<Home />} />
+        <Route path="/stories" element={<Home />} />
+        {/* и т.д. */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
