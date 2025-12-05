7f710ed5093a8ea48082600396d939fe90364deb
ekbatyaev <161816517+ekbatyaev@users.noreply.github.com>
2025-12-05 23:25:10 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   base: '/apis-storage/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/html',
+          dest: 'src'
+        }
+      ]
+    })
+  ],
   css: {
     postcss: './postcss.config.js',
   },
