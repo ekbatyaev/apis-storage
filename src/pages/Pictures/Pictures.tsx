e483cdbcfc3e243f4089526e0d39577523be3405
ekbatyaev <s1ntenkov13@gmail.com>
2025-12-15 14:43:35 +0300

    updates

--- src/pages/Pictures/Pictures.tsx
+++ src/pages/Pictures/Pictures.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styles from './Pictures.module.scss';
 
 export default function Pictures() {
--- src/pages/Places/Places.tsx
+++ src/pages/Places/Places.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styles from './Places.module.scss';
 
 interface PlaceItem {
